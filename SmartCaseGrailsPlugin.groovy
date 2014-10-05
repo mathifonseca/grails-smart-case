25ef668d882dc4cba8d54298fa4b7c118c578538
Mathi Fonseca <mathifonseca@gmail.com>
2014-10-05 19:33:29 -0200

    initial grails project

---
+++ SmartCaseGrailsPlugin.groovy
@@ -0,0 +1,14 @@
+class SmartCaseGrailsPlugin {
+    def version = "0.0.1"
+    def grailsVersion = "2.3 > *"
+    def title = "Smart Case Plugin"
+    def author = "Mathias Fonseca"
+    def authorEmail = "mathifonseca@gmail.com"
+    def description = '''\
+This plugin provides an easy way to convert between cases for Strings and variable names
+'''
+    def documentation = "http://grails.org/plugin/smart-case"
+    def license = "APACHE"
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/mathifonseca/grails-smart-case/issues" ]
+    def scm = [ url: "https://github.com/mathifonseca/grails-smart-case" ]
+}
---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,21 @@
+grails.project.work.dir = 'target'
+
+grails.project.dependency.resolution = {
+
+    inherits 'global'
+    log 'warn'
+
+    repositories {
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+    }
+
+    plugins {
+        build(":release:3.0.1",
+              ":rest-client-builder:2.0.3") {
+            export = false
+        }
+    }
+    
+}
