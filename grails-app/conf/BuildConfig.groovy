3d8629caafd2eba0e699dab794e47ffaf09b5746
Mathi Fonseca <mathifonseca@gmail.com>
2014-10-08 13:18:44 -0200

    added version-update plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,8 +13,12 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ':release:3.0.1', ':rest-client-builder:2.0.3' {
+        build(':release:3.0.1', ':rest-client-builder:2.0.3') {
+            export = false
+        }
+        provided(':version-update:1.3.2') {
             export = false
         }
     }
+    
 }
