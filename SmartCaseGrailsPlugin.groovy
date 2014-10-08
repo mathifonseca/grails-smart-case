f0fb220b34bc4413e7d835d36aec138944ad8f13
Burt Beckwith <burt@burtbeckwith.com>
2014-10-07 23:20:22 -0400

    cleanup

--- SmartCaseGrailsPlugin.groovy
+++ SmartCaseGrailsPlugin.groovy
@@ -1,6 +1,4 @@
 import grails.util.Environment
-import groovy.util.ConfigObject
-import groovy.util.ConfigSlurper
 
 class SmartCaseGrailsPlugin {
     def version = "0.0.1"
@@ -8,9 +6,7 @@ class SmartCaseGrailsPlugin {
     def title = "Smart Case Plugin"
     def author = "Mathias Fonseca"
     def authorEmail = "mathifonseca@gmail.com"
-    def description = '''\
-This plugin provides an easy way to convert between cases for Strings and variable names
-'''
+    def description = 'Provides an easy way to convert between cases for Strings and variable names'
     def documentation = "http://grails.org/plugin/smart-case"
     def license = "APACHE"
     def issueManagement = [ system: "GITHUB", url: "https://github.com/mathifonseca/grails-smart-case/issues" ]
@@ -25,12 +21,11 @@ This plugin provides an easy way to convert between cases for Strings and variab
     }
 
     private void mergeConfig(app) {
-        ConfigObject currentConfig = app.config.smartCase
-        ConfigSlurper slurper = new ConfigSlurper(Environment.getCurrent().getName())
+        ConfigSlurper slurper = new ConfigSlurper(Environment.current.name)
         ConfigObject secondaryConfig = slurper.parse(app.classLoader.loadClass("SmartCaseConfig"))
         ConfigObject config = new ConfigObject()
+        ConfigObject currentConfig = app.config.smartCase
         config.putAll(secondaryConfig.smartCase.merge(currentConfig))
         app.config.smartCase = config
     }
-
 }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,8 +1,8 @@
 log4j = {
-  	error 	'org.codehaus.groovy.grails',
-        	'org.springframework',
-        	'org.hibernate',
-        	'net.sf.ehcache.hibernate'
+	error 'org.codehaus.groovy.grails',
+	      'org.springframework',
+	      'org.hibernate',
+	      'net.sf.ehcache.hibernate'
 }
 
 // EXAMPLE CONFIGURATION
